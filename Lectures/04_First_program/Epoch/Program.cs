29d54d12e1e7d2e68da73420545d1533b64dc26d
MatttyHlo <mahlo25@student.sdu.dk>
2025-09-12 13:08:52 +0200

    Adding Exircises for the Lecture

---
+++ Lectures/04_First_program/Epoch/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+int seconds = 1757674303;
+
+int years = seconds / (365 * 24 * 3600);
+int days = (seconds - years * 365 * 24 * 3600) / (24 * 3600);
+
+Console.WriteLine($"year: {1970 + years} day: {days}");
---
+++ Lectures/04_First_program/Temperatures/Program.cs
@@ -0,0 +1,15 @@
+﻿double monday = 21.5;
+double tuesday = 23.7;
+double wednesday = 19.6;
+double thursday = 22.5;
+double friday = 25.3;
+double saturday = 21.7;
+double sunday = 18.9;
+
+Console.WriteLine(tuesday - monday);
+Console.WriteLine(wednesday - tuesday);
+Console.WriteLine(thursday - wednesday);
+Console.WriteLine(friday - thursday);
+Console.WriteLine(saturday - friday);
+Console.WriteLine(sunday - saturday);
+
