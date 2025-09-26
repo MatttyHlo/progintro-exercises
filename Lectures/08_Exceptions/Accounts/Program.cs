e0ea221d4f4eaddc34ae8fad8eadfc64be5e3843
MatttyHlo <mahlo25@student.sdu.dk>
2025-09-26 13:29:20 +0200

    New Lecture

---
+++ Lectures/08_Exceptions/Accounts/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] accounts = {903, 716, 67};
+
+int GetAccountNumber ()
+{
+    Console.WriteLine("Enter an account number: ");
+    int num = -1;
+
+    while(num == -1){
+
+        try {
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+
+            if (inputNum < 0 || inputNum >= 3)
+            {
+                Console.WriteLine("Invalid input, try again:");
+            }
+            else
+            {
+                num = inputNum;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Invalid input, try again:");
+        }
+    }
+
+    return num;
+}
+
+void PrintAccountState (int accountId)
+{
+    Console.WriteLine("Account " + accountId + " contains " + accounts[accountId]);
+}
+
+while (true) {
+    int accountId = GetAccountNumber();
+    PrintAccountState(accountId);
+}
---
+++ Lectures/08_Exceptions/AverageGrade/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] grades = {4, 7, 02, 00, 10, 4, 12};
+
+int GetGrade(int courseid, int[] grades){
+    int grade = grades[courseid];
+
+    if(grade <= 2)throw new Exception("Failing grade");
+    else return grade;
+}
+int count = 0;
+int sum = 0;
+int grade;
+for(int courseid = 0; courseid < grades.Length; courseid++){
+    try{
+        grade = GetGrade(courseid, grades);
+    }
+    catch {continue;}
+    count++;
+    sum += grade;
+
+}
+Console.WriteLine((double)sum/count);
